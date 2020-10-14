1baf151bc17c6d89f5ccbeb96b20640b0a6058d3
Renata <chromic507@gmail.com>
2020-10-14 20:27:27 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "view.h"
 
 #include <QApplication>
-
+#include <controller.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     View w;
+
+    Controller *controller = new Controller();
+    w.setFigureMovementListener(controller);
+
     w.show();
     return a.exec();
 }
